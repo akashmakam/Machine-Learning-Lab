8b1876785d6e620c9d113f13e2d07118bf7d680c
akashmakam <makamakash11@gmail.com>
2025-08-29 05:24:18 +0000

    .

--- 1.py
+++ 1.py
@@ -1,3 +1,23 @@
+# 2D Scatter Plot
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# df = pd.read_csv("cancer.csv")
+# x = df['Radius (mean)']
+# y = df['Texture (mean)']
+
+x = np.random.randn(100)
+y = np.random.randn(100)
+
+plt.scatter(x, y)
+plt.title("2D Scatter Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
+
+
+# 3D Scatter Plot
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
